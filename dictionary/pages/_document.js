2359a2cde60c1e82a8e87a56e6ed9d6530915daa
Nnamdi Okeke <onnamdialex@gmail.com>
2023-04-04 14:32:58 +0100

    :bug: fix the reference error bug

--- dictionary/pages/_document.js
+++ dictionary/pages/_document.js
@@ -1,6 +1,6 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
-export default function Document() {
+function Document() {
   return (
     <Html lang="en">
       <Head>
@@ -16,9 +16,52 @@ export default function Document() {
         />
       </Head>
       <body>
+        <script
+          dangerouslySetInnerHTML={{ __html: themeInitializerScript }}
+        ></script>
         <Main />
         <NextScript />
       </body>
     </Html>
   );
 }
+
+//creating the theme initializer function
+async function themeInitializerScript() {
+  await `(() => {
+  ${setInitialColorMode.toString()}
+  setInitialColorMode();
+})`;
+}
+
+const setInitialColorMode = () => {
+  //checking the initial color preference
+  const getInitialColorMode = () => {
+    const persistedPreferenceMode = window.localStorage.getItem("theme");
+    const hasPersistedPreference = typeof persistedPreferenceMode === "string";
+
+    if (hasPersistedPreference) {
+      return persistedPreferenceMode;
+    }
+
+    // Check the current preference
+    const preference = window.matchMedia("(prefers-color-scheme: dark)");
+    const hasMediaQueryPreference = typeof preference.matches === "boolean";
+
+    if (hasMediaQueryPreference) {
+      return preference.matches ? "dark" : "light";
+    }
+
+    return "light";
+  };
+
+  const currentColorMode = getInitialColorMode();
+  const element = document.documentElement;
+  element.style.setProperty("--initial-color-mode", currentColorMode);
+
+  // If darkmode apply darkmode
+  if (currentColorMode === "dark")
+    document.documentElement.setAttribute("data-theme", "dark");
+};
+
+export default Document;
