1e7a0a4be3abcde114f8527f9ddd9b841b841d86
Nnamdi Okeke <onnamdialex@gmail.com>
2023-05-15 19:54:36 +0100

    :bulb: update comments

--- dictionary/components/Dictionary.jsx
+++ dictionary/components/Dictionary.jsx
@@ -37,7 +37,7 @@ const Dictionary = () => {
     }
   }, [debouncedValue]);
 
-  //the effect hook to handle the (re)rendering of the the themes
+  //the effect hook to handle the (re)rendering of the themes
   useEffect(() => {
     if (darkTheme !== undefined) {
       if (darkTheme) {
